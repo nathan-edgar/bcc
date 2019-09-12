766e7a533068441cec22d2ba65c6ebc66fdd9f8c
Zwb <ethercflow@gmail.com>
2019-09-11 23:53:31 -0400

    update iothrottle: support timestamp

--- tools/iothrottle.py
+++ tools/iothrottle.py
@@ -1,8 +1,33 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+# @lint-avoid-python-3-compatibility-imports
+#
+# iothrottle  Trace balance_dirty_pages's io throttle's latency and print
+# details including issuing PID.
+#       For Linux, uses BCC, eBPF.
+#
+# Copyright (c) 2019 Ethercflow
+# Licensed under the Apache License, Version 2.0 (the "License")
+#
+# 12-Sep-2019   Ethercflow   Created this.
 
 from __future__ import print_function
 from bcc import BPF
+import argparse
 
+# arguments
+examples = """examples:
+    ./iothrottle          # trace all io throttle
+    ./iothrottle -T       # include timestamps
+"""
+parser = argparse.ArgumentParser(
+    description="Trace io throttle",
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    epilog=examples)
+parser.add_argument("-T", "--timestamp", action="store_true",
+                    help="include timestamp on output")
+args = parser.parse_args()
+
+# load BPF program
 bpf_test = """
 #include <uapi/linux/ptrace.h>
 #include <linux/sched.h>
@@ -26,7 +51,8 @@ struct val_t {
 BPF_HASH(start, u64, struct val_t);
 BPF_PERF_OUTPUT(events);
 
-int trace_vfs_write_entry(struct pt_regs *ctx, struct file *file, const char __user *buf, size_t count, loff_t *pos)
+int trace_vfs_write_entry(struct pt_regs *ctx, struct file *file,
+                          const char __user *buf, size_t count, loff_t *pos)
 {
     struct val_t val = {};
     u64 id = bpf_get_current_pid_tgid();
@@ -37,7 +63,8 @@ int trace_vfs_write_entry(struct pt_regs *ctx, struct file *file, const char __u
         val.pid = pid;
         val.tid = tid;
         val.ts = bpf_ktime_get_ns();
-        bpf_probe_read_str(&val.fname, sizeof(val.fname), file->f_path.dentry->d_name.name);
+        bpf_probe_read_str(&val.fname, sizeof(val.fname),
+                           file->f_path.dentry->d_name.name);
         val.count = count;
         val.pos = *pos;
 
@@ -52,7 +79,7 @@ TRACEPOINT_PROBE(writeback, balance_dirty_pages) {
     struct val_t *valp;
 
     valp = start.lookup(&id);
-    if (valp == NULL) 
+    if (valp == NULL)
         return 0;
 
     if (args->paused > 0)
@@ -93,16 +120,27 @@ b = BPF(text=bpf_test)
 b.attach_kprobe(event="vfs_write", fn_name="trace_vfs_write_entry")
 b.attach_kretprobe(event="vfs_write", fn_name="trace_vfs_write_return")
 
+initial_ts = 0
+
 # header
+if args.timestamp:
+    print("%-14s" % ("TIME(s)"), end="")
 print("%-14s %-6s %-6s %-20s %-12s %-12s %-6s %-8s %-16s %12s %12s"
       % ("COMM", "PID", "TID", "FNAME", "COUNT", "POS",
          "BDI", "DIRTIED", "RATELIMIT(KBps)", "PAUSED(ms)", "LAT(ms)"))
 
 # process event
-
-
 def print_event(cpu, data, size):
     event = b["events"].event(data)
+
+    global initial_ts
+
+    if not initial_ts:
+        initial_ts = event.ts
+
+    if args.timestamp:
+        delta = event.ts - initial_ts
+        print("%-14.9f" % (float(delta) / 1000000), end="")
 
     print("%-14s %-6s %-6s %-20s %-12s %-12s %-6s %-8s %-16d %12d %12.2f" %
           (event.name.decode('utf-8', 'replace'),
@@ -115,7 +153,7 @@ def print_event(cpu, data, size):
            event.paused,
            float(event.delta) / 1000000))
 
-
+# loop with callback to print_event
 b["events"].open_perf_buffer(print_event, page_cnt=64)
 while 1:
     try:
